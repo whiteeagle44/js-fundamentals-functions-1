b6611ce79f61997fd21eb9bcf1006e3a5107a61c
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-19 12:40:04 +0200

    exercises

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function arrayFromLowerAndUpper(lower, upper) {
+  const element = []
+  for (let i = lower; i <= upper; i++) {
+    element.push(i)
+  }
+  return element
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,11 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function changedString(str, num) {
+  const upperString = str.toUpperCase();
+  const exclamation = "!".repeat(num);
+  return upperString + exclamation;
+  }
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,6 +45,25 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function newTime(time, addedMinutes) {
+  let timearr = time.split(':')
+  let hours = Number(timearr[0])
+  let minutes = timearr[1]
+  let newMinutes = Number(minutes) + addedMinutes
+  let newHours = hours
+
+  while (newMinutes >= 60) {
+    newMinutes -= 60
+    newHours += 1
+  }
+  newHours = newHours % 24
+  let finalHours = String(newHours)
+  if (finalHours === '0') {
+    finalHours = '00'
+  }
+  const finalMinutes = String(newMinutes).padStart(2, '0')
+  return `${finalHours}:${finalMinutes}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
